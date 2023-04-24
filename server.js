59483bcc4005cfaa5bb91140ca99edb230b9d0a9
salfra <salfra@email.unc.edu>
2023-04-24 15:42:13 -0400

    added quotation mark

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 //IMPORT
 import express from "express"
 import minimist from "minimist"
-import { rps, rpsls } from "./lib/rpsls.js
+import { rps, rpsls } from "./lib/rpsls.js"
 
 //VARIABLE
 var argv = minimist(process.argv.slice(2));
